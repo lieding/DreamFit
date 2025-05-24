52d65a3a6ad9f2fee38e744e857b016a5613afc4
LieDING <ding@getmicco.com>
2025-05-25 00:37:04 +0200

    aa

--- run_inference_dreamfit_sd15_i2i.sh
+++ run_inference_dreamfit_sd15_i2i.sh
@@ -2,6 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 python3 inference_dreamfit_sd15_i2i.py \
     --config configs/inference/inference_dreamfit_sd15_i2i.yaml \
-    --vae_ckpt pretrained_models/sd-vae-ft-mse/diffusion_pytorch_model.bin \
-    --ref_model pretrained_models/sd15_i2i.ckpt \
+    --vae_ckpt /home/featurize/work/pretrained_models/diffusion_pytorch_model.bin \
+    --ref_model /home/featurize/work/pretrained_models/sd15_i2i.ckpt \
+    --base_model /home/featurize/work/app/comfyui/ComfyUI/models/checkpoints/epicphotogasm_ultimateFidelity.safetensors \
     "$@"
